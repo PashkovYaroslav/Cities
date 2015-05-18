cc15685f896ac8d000a93b25b55ac8543e1043a5
Yaroslav Pashkov <Yaroslav_Pashkov@epam.com>
2015-05-18 12:56:13 +0300

    Added log messages

--- src/main/java/com/epam/pashkov/players/Computer.java
+++ src/main/java/com/epam/pashkov/players/Computer.java
@@ -1,6 +1,7 @@
 package com.epam.pashkov.players;
 
 import com.epam.pashkov.datasource.CitiesDAO;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,13 +10,17 @@ import java.util.List;
  * Created by Yaroslav_Pashkov on 5/15/2015.
  */
 public class Computer implements Player {
+    public static final Logger CITIES_TEST_LOG = Logger.getLogger(Computer.class);
+
     public String sayCity(CitiesDAO citiesData, List<String> history, String city, String prewCity) {
         if (prewCity.charAt(prewCity.length() - 1) == 'ы' || prewCity.charAt(prewCity.length() - 1) == 'ъ' || prewCity.charAt(prewCity.length() - 1) == 'ь') {
+            CITIES_TEST_LOG.info("The last char is 'ы', 'ъ', 'ь'");
             char prewChar = Character.toUpperCase(prewCity.charAt(prewCity.length() - 2));
             return findCityInDataSource(citiesData, history, prewChar);
         }
 
         else {
+            CITIES_TEST_LOG.info("The last char isn't 'ы', 'ъ', 'ь'");
             char prewChar = Character.toUpperCase(prewCity.charAt(prewCity.length() - 1));
             return findCityInDataSource(citiesData, history, prewChar);
         }
@@ -28,6 +33,7 @@ public class Computer implements Player {
         for (String city : citiesList) {
             if (city.charAt(0) == prewChar) {
                 if (!history.contains(city)) {
+                    CITIES_TEST_LOG.info("History not contains "+city);
                     newCity = city;
                     break;
                 }
--- src/main/java/com/epam/pashkov/players/People.java
+++ src/main/java/com/epam/pashkov/players/People.java
@@ -1,6 +1,7 @@
 package com.epam.pashkov.players;
 
 import com.epam.pashkov.datasource.CitiesDAO;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
@@ -8,19 +9,25 @@ import java.util.List;
  * Created by Yaroslav_Pashkov on 5/15/2015.
  */
 public class People implements Player {
+    public static final Logger CITIES_TEST_LOG = Logger.getLogger(Computer.class);
+
     public String sayCity(CitiesDAO citiesData, List<String> history, String city, String prewCity) throws StringIndexOutOfBoundsException {
         if (prewCity.charAt(prewCity.length() - 1) == 'ы' || prewCity.charAt(prewCity.length() - 1) == 'ъ' || prewCity.charAt(prewCity.length() - 1) == 'ь') {
+            CITIES_TEST_LOG.info("The last char is 'ы', 'ъ', 'ь'");
             if (city.charAt(0) == Character.toUpperCase(prewCity.charAt(prewCity.length() - 2))) {
                 if (citiesData.getCity(city)) {
                     if (!history.contains(city)) {
+                        CITIES_TEST_LOG.info("History not contains "+city);
                         return city;
                     }
                 }
             }
         } else {
+            CITIES_TEST_LOG.info("The last char isn't 'ы', 'ъ', 'ь'");
             if (city.charAt(0) == Character.toUpperCase(prewCity.charAt(prewCity.length() - 1))) {
                 if (citiesData.getCity(city)) {
                     if (!history.contains(city)) {
+                        CITIES_TEST_LOG.info("History not contains "+city);
                         return city;
                     }
                 }
